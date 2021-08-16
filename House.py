eff50411acff1b8536f89044cd759d1d9c5a00ae
Darrell Leong <a843183@yara.com>
2021-08-16 08:06:37 +0800

    added draft return framework

--- House.py
+++ House.py
@@ -1,4 +1,3 @@
-import numpy as np
 import numpy_financial as npf
 import copy
 import os
@@ -8,10 +7,12 @@ PWD = os.path.abspath(os.path.dirname(__file__))
 
 
 class House:
-    def __init__(self, unitPrice, reno):
+    def __init__(self, unitPrice, reno, mortInt, mortTerm):
         self.confDict = json.load(fp=open(f"{PWD}/conf.json", "r"))
         self.unitPrice = unitPrice
         self.reno = reno
+        self.mortInt = mortInt
+        self.mortTerm = mortTerm
 
         self.downPmt = self.confDict["downPmtPortion"] * unitPrice
         self.mortgageLiability = unitPrice - self.downPmt
@@ -48,26 +49,44 @@ class House:
         pmtMonthly = -npf.pmt(rate=intMonthly, nper=termMonths, pv=principal, fv=0, when="begin")
         return pmtMonthly * 12
 
-    def getAnnualCF(self, availCPF, mortInt, mortTerm, share=1, downPmtRatio=1, addloan=0, addInt=0.02, addTerm=10):
+    def _getMortLia(self, share, downPmtRatio, addloan):
         asset = share * self.unitPrice
         downPmtShare = downPmtRatio * self.downPmt
         mortLiaShare = asset - downPmtShare - addloan
-        mortCF = self._getAnnualPmt(principal=mortLiaShare, intAnnual=mortInt, termYears=mortTerm)
+        return mortLiaShare
+
+    def getAnnualCF(self, availCPF, share=1, downPmtRatio=1, addloan=0, addInt=0.02, addTerm=10):
+        mortLiaShare = self._getMortLia(share=share, downPmtRatio=downPmtRatio, addloan=addloan)
+        mortCF = self._getAnnualPmt(principal=mortLiaShare, intAnnual=self.mortInt, termYears=self.mortTerm)
         addCF = self._getAnnualPmt(principal=addloan, intAnnual=addInt, termYears=addTerm)
         return max(mortCF + addCF - availCPF, 0)
 
-    def getFV(self, years, ror, share=1):
+    def _getAssetFV(self, years, ror, share=1):
         FVIF = (1 + ror) ** years
         return FVIF * share * self.unitPrice
 
+    def getCashReturn(self, availCPFUpfront, availCPFAnnual, years, ror, share=1):
+        assetFV = self._getAssetFV(years=years, ror=ror, share=share)
+        cpfFV = -npf.fv(rate=self.confDict["cpfReturn"], nper=years, pmt=availCPFAnnual,
+                        pv=availCPFUpfront, when="begin")
+
+    # def getLiabilityFV(self, years, share=1, downPmtRatio=1, addloan=0, addInt=0.02):
+    #     asset = share * self.unitPrice
+    #     downPmtShare = downPmtRatio * self.downPmt
+    #     mortLiaShare = asset - downPmtShare - addloan
 
 
 
 if __name__ == "__main__":
     unitPrice = 1.08e6
     reno = 6e4
-    ins = House(unitPrice=unitPrice, reno=reno)
-    upfrontCF = ins.getUpfrontCF(availCPF=0.14e6, share=0.4, downPmtRatio=0.5)
+    mortInt = 0.015
+    mortTerm = 30
+    share = 0.04
+    downPmtRatio = 0.5
+    ins = House(unitPrice=unitPrice, reno=reno, mortInt=mortInt, mortTerm=mortTerm)
+    upfrontCF = ins.getUpfrontCF(availCPF=0.14e6, share=share, downPmtRatio=downPmtRatio)
     print("Upfront cashflow: %.2f" % upfrontCF)
-    annualCF = ins.getAnnualCF(availCPF=9600, mortInt=0.015, mortTerm=30, share=0.4, downPmtRatio=0.5)
+    annualCF = ins.getAnnualCF(availCPF=9600, share=share, downPmtRatio=downPmtRatio)
     print("Annual cashflow: %.2f" % annualCF)
+    cashReturn = ins.getAssetReturn(years=6, ror=0.04, share=share)
---
+++ Owner.py
@@ -0,0 +1,11 @@
+import numpy_financial as npf
+
+
+class Owner:
+    def __init__(self, availCashUpfront, availCashAnnual):
+        self.availUpfront = availCashUpfront
+        self.availAnnual = availCashAnnual
+
+
+if __name__ == "__main__":
+    pass
